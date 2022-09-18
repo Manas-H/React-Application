e45f544a31c1f98d2117c5f778b031daac8214c0
Vedantk1301 <vedk1301@gmail.com>
2022-09-19 02:18:00 +0530

    Added animation layer

--- frontend/src/components/pages/chooseus.js
+++ frontend/src/components/pages/chooseus.js
@@ -2,20 +2,28 @@ import React from 'react';
 //import images from '../images/bg.png';
 import classes from './chooseus.module.css';
 import Fade from 'react-reveal/Fade';
+import images from '../images/ad.jpeg';
+//import comb from '../images/comb.png';
 
 const Choose = () => { 
 
     return (
-        //style={{backgroundImage:`url(${images})` }}
+       <>
         <div className={classes.chdiv}>
+            <div className={classes.container}>
+            <div className={classes.bannerdiv}>
+            </div>
+            </div>
+            </div>
             <Fade duration={2000} delay={200}>
                 <div className={classes.div2}>
             <div className={classes.headdiv}>
-            </div>
+                <img className={classes.imgc}src={images} alt="Why choose us"></img>
             <h1 className={classes.head1}>WHY CHOOSE US!!</h1>
             </div>
+            </div>
             </Fade>
-          </div>
+            </>
            );
  }
 
