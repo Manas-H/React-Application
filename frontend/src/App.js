4d192515af2f1ada52d553e91e75b5bac62c2143
Vedantk1301 <vedk1301@gmail.com>
2022-09-18 14:19:25 +0530

    Homepage slider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
-// import Navigationbar from "./components/Navigationbar";
+//import Navigationbar from "./components/Navigationbar";
 import { Routes, Route } from "react-router-dom";
 import About from "./components/pages/About";
 import Contact from "./components/pages/Contact"
 import Home from "./components/pages/Home"
 
+
 function App() {
   return (
+
     <div>
-      {/* <Navigationbar /> */}
+         
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/about" element={<About/>}></Route>
         <Route path="/contact" element={<Contact/>}></Route>
-      </Routes>
+      </Routes> 
     </div>
-  );
-}
+);}
 
 export default App;
