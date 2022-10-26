473cfb9de50d03b6024155565c0cb22ba3499fc5
Vedantk1301 <vedk1301@gmail.com>
2022-10-26 23:57:05 +0530

    New overall layout

--- frontend/src/components/products/Rectangle.js
+++ frontend/src/components/products/Rectangle.js
@@ -2,9 +2,9 @@ import React from "react";
 import Navigationbar from "../Navigationbar";
 import classes from "../styles/Rectangle.module.css";
 import rect from "../images/Sandal.png";
-// import newz from "../images/newz.png";
-import newz2 from "../images/newz2.jpg";
-import fsh from "../images/fsh.jpg";
+ //import newz from "../images/newz.png";
+import newz2 from "../images/rectp1.jpg";
+import fsh from "../images/rectp2.jpg";
 import { useRef } from 'react'
 import Product from "./AllProducts";
 import {
@@ -14,28 +14,35 @@ import {
 const Rectangle= (props) =>{
   const firstItemRef = useRef(null);
     return (
-   <div>
+   <div className={classes.MainDIV}>
     <Navigationbar/>
-    <div className="main-body2">
+   
       <div className ={classes.header}>
-        <h1 className={classes.title}>THE <br/>Rectangular Series</h1>
-        <div className={classes.Imagediv}>
+        <div className={classes.contents}>
+        <h1 className={classes.title}>The Rectangular Series</h1>
+        <h4 className={classes.desc}> Our Premium rectangular planters ideal for your Home, Garden and Lawn decorations. Elegant and modern designs, they are ideal for both indoor and outdoor use, fitting well with contemporary architecture styles.</h4>
+        </div>
+        <div className={classes.imgcontent}>
         <img className={classes.mainimg} src= {rect} alt ="Rectangle"></img>
+        </div>
+        </div>
+       <div className={classes.twodiv}>
+        <div className={classes.disdiv}>
+         <img className={classes.mainimg3} src ={newz2} alt ="rectangular fiberglass"></img>
+        <img className={classes.mainimg3} src ={fsh} alt ="rectangular fiberglass"></img>
+        </div>
+        <div className={classes.browse}>
         <h3 className={classes.br}> BROWSE PRODUCTS</h3>
-        <button className={classes.btnfa} onClick={() => firstItemRef.current.scrollIntoView()}><FaArrowCircleDown style={{fontSize: 80, fontWeight: 200,}} /></button>
-        {/* <img className={classes.mainimg3} src ={newz} alt ="rectangular fiberglass"></img> */}
+        <button className={classes.btnfa} onClick={() => firstItemRef.current.scrollIntoView()}><FaArrowCircleDown style={{fontSize: 80, fontWeight: 200,}} /></button> 
         </div>
-        {/* <img className={classes.mainimg} src={(Name.Image1)} alt = "nothing"></img> */}
         </div>
-        <div className={classes.ent}>
-        <h4 className={classes.desc}> Our Premium rectangular planters ideal for your Home, Garden and Lawn decorations!!</h4>
-        {/* <h6 className={classes.des}>Sturdy and widespread horizontal planters precisely made to give you more area for your plants </h6> */}
-        <img className={classes.mainimg3} src ={newz2} alt ="rectangular fiberglass"></img>
-        <img className={classes.mainimg2} src ={fsh} alt ="rectangular fiberglass"></img>
-        </div>
+         
        
-   </div>
-   <div ref={firstItemRef}></div>
+
+   <div ref={firstItemRef} className={classes.prd}></div>
+   
+   <h1 className={classes.prdtitle} >PRODUCTS</h1>
+   <hr className={classes.line}></hr>
    <Product value={1}/>
    </div>
     );
